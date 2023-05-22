79f8f77df15781446bd7dd9643c2094c162319e0
han22232 <109938669+han22232@users.noreply.github.com>
2023-05-22 21:14:57 +0930

    Add files via upload

---
+++ klargest.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<queue>
+using namespace std;
+
+int kth_largest(vector<int> values, int k) {
+  priority_queue<int, vector<int>, greater<int>> pq;
+  for (int i = 0; i < values.size(); i++) {
+    pq.push(values.at(i));
+    if (pq.size() > k) {
+      pq.pop();
+    }
+  }
+  return pq.top();
+}
